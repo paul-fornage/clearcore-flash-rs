2484af3c3293a6db21db7372c09dbf4a98c69da1
Paul Fornage <paul@fornage.net>
2026-01-21 15:45:33 -0800

    God I hate windows pt 2 electric bugaloo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+// makes windows not launch the EXE with a terminal
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
+
+
 mod app;
 mod serial;
 mod types;
